a991158e8ef5421029c2ead5aca9dad4cb36447e
Lucas <lucasrech00@gmail.com>
2025-04-22 14:57:34 -0300

    feat: instanciado mocks de produtos e melhorias de Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,22 @@
 import './App.css'
 import Card from './components/Card'
+import mock from './services/mock.json'
 
 function App() {
+  const flavors = mock.flavors
 
 
   return (
     <div className="flex align-center justify-center flex-wrap gap-10">
-      <Card
-        title='Morango'
-        description='Bolo de morango com cobertura de chocolate'
-        price={5.99}
-        imageUrl='https://cdn.pixabay.com/photo/2016/03/05/19/02/strawberry-1238250_1280.jpg' 
-      />
-      <Card
-        title='Chocolate'
-        description='Bolo de chocolate com cobertura de morango'
-        price={5.99}
-        imageUrl='https://cdn.pixabay.com/photo/2016/03/05/19/02/strawberry-1238250_1280.jpg' 
-      />
-      <Card
-        title='Abacaxi'
-        description='Bolo de abacaxi com cobertura de chocolate'
-        price={5.99}
-        imageUrl='https://cdn.pixabay.com/photo/2016/03/05/19/02/strawberry-1238250_1280.jpg' 
-      />
-      
+      {flavors.map((flavor, id) => (
+        <Card
+          key={id}
+          title={flavor.flavor}
+          description={flavor.description}
+          imageUrl={flavor.image}
+          price={flavor.price}
+        />
+      ))}
     </div>
   )
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,14 +7,19 @@ interface CardProps {
 
 
 function Card( { title, description, price, imageUrl }: CardProps) {
+    const formattedPrice = price.toFixed(2).replace('.', ',')
+
+
     return (
-        <div className="card bg-primary mb-3" style={{ width: '18rem' }}>
-            <img className="card-img-top" src={imageUrl} alt="Card image cap" /> 
+        <div className="card bg-primary mb-3 w-[18rem] max-h-[28rem]">
+            <div className="h-[40%] ">
+                <img className="card-img-top w-fit h-full object-cover" src={imageUrl} alt="Card image cap" /> 
+            </div>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <p className="card-price text-2xl"><strong>R$ {price}</strong></p>
-                <a href="#" className="btn btn-secondary">Go somewhere</a>
+                <p className="card-text h-[33%]">{description}</p>
+                <p className="card-price text-2xl"><strong>R$ {formattedPrice}</strong> <span className="text-sm">Kg</span></p>
+                <a href="#" className="btn btn-secondary">Encomendar</a>
             </div>
         </div>
     );
